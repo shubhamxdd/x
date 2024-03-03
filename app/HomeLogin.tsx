56375d04b47282d04129d5dcd6b99002fd920dee
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-03 20:20:51 +0530

    Link login and register modal

--- app/HomeLogin.tsx
+++ app/HomeLogin.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import useLoginModal from "@/hooks/useLoginModal";
+
 const HomeLogin = () => {
+  const { open } = useLoginModal();
   return (
     <div className="mt-20">
       <h2 className="text-white text-xl font-bold">Already have an account?</h2>
-      <div className="bg-black text-white px-5 py-2 rounded-full text-center border-[0.5px] border-sky-700 hover:bg-sky-500 hover:bg-opacity-10 transition cursor-pointer mt-4 w-[75%] md:w-[50%] lg:w-[40%] xl:w-[30%] 2xl:w-[20%]">
+      <div
+        onClick={open}
+        className="mt-5 w-[75%] md:w-[50%] lg:w-[50%] xl:w-[40%] 2xl:w-[20%] bg-black text-white px-5 py-2 rounded-full text-center border-[0.5px] border-sky-700 hover:bg-sky-500 hover:bg-opacity-10 transition cursor-pointer"
+      >
         <p>Sign in</p>
       </div>
     </div>
--- app/HomeSignup.tsx
+++ app/HomeSignup.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import SignupButton from "@/components/SignupButton";
+import useRegisterModal from "@/hooks/useRegisterModal";
 import { FaApple } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 
 const HomeSignup = () => {
+  const { open } = useRegisterModal();
   const signupButtons = [
     { label: "Google", icon: FcGoogle },
     { label: "Apple", icon: FaApple },
@@ -14,7 +18,10 @@ const HomeSignup = () => {
           <SignupButton key={item.label} label={item.label} icon={item.icon} />
         ))}
       </div>
-      <div className="bg-sky-500 text-white px-5 py-2 rounded-full mt-5 w-[75%] md:w-[50%] lg:w-[50%] xl:w-[40%] 2xl:w-[20%] text-center font-bold cursor-pointer">
+      <div
+        onClick={open}
+        className="bg-sky-500 text-white px-5 py-2 rounded-full mt-5 w-[75%] md:w-[50%] lg:w-[50%] xl:w-[40%] 2xl:w-[20%] text-center font-bold cursor-pointer"
+      >
         <p>Create Account</p>
       </div>
       <div className="w-[75%] md:w-[50%] lg:w-[50%] xl:w-[40%] 2xl:w-[20%]">
