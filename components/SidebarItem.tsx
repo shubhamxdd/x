176e55ecd9e22f736dbf14c8eb5efb4493ada74c
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-03 13:50:43 +0530

    reduce height

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -10,7 +10,7 @@ interface Props {
 const SidebarItem = ({ href, icon: Icon, label }: Props) => {
   return (
     <div className="flex flex-row items-center">
-      <div className="relative rounded-full h-[44px] w-14 flex items-center justify-center p-2 hover:bg-neutral-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+      <div className="relative rounded-full h-11 w-14 flex items-center justify-center p-2 hover:bg-neutral-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
         <Icon size={24} color="white" />
       </div>
       <div className="relative hidden lg:flex items-center gap-4 p-2 rounded-full hover:bg-neutral-300 hover:bg-opacity-10 cursor-pointer">
