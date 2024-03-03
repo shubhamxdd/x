385530066fc58078aa6be0bf9a076cc48f423e82
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-03 16:46:35 +0530

    make landing page and move main page to "/home" route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import UILayout from "@/components/UILayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <UILayout>{children}</UILayout>
+        {children}
       </body>
     </html>
   );
