7e4bcf1aae53caa081e51700bacce6ff925f57a7
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-04 20:56:08 +0530

    add toast provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import LoginModal from "@/components/Modals/LoginModal";
 import RegisterModal from "@/components/Modals/RegisterModal";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster />
         <LoginModal />
         <RegisterModal />
         {children}
